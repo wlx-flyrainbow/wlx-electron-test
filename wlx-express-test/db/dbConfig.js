3ed7c90d650dcb303a330dc11152e3adf97cf6b1
万乐祥 <wanlexiang@ucfgroup.com>
2018-02-02 19:23:55 +0800

    mysql 连接成功，插入数据成功

--- wlx-express-test/db/dbConfig.js
+++ wlx-express-test/db/dbConfig.js
@@ -4,8 +4,8 @@ module.exports =
     mysql: {   
         host: 'localhost',     
         user: 'root',   
-        password: '你的数据库Coonection密码',  
+        password: '101617',  
         database:'ExpressDB', // 前面建的user表位于这个数据库中 
-        port: 3000 
+        port: 3306
     }
  };
--- wlx-express-test/db/userSql.js
+++ wlx-express-test/db/userSql.js
@@ -3,5 +3,5 @@ var UserSQL = {
     insert:'INSERT INTO User(uid,userName) VALUES(?,?)', 
     queryAll:'SELECT * FROM User',  
     getUserById:'SELECT * FROM User WHERE uid = ? '
-};
-module.exports = UserSQL;
+}
+module.exports = UserSQL
